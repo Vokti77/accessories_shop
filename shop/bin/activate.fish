6c323eb0c83a47c8eb238982034e08bc276fc906
EMNayon <emranmasudnayon.017@gmail.com>
2023-05-28 18:17:04 +0600

    update: design updated

--- shop/bin/activate.fish
+++ shop/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nayon/Desktop/AinoviQ/accessories_shop/shop"
+set -gx VIRTUAL_ENV "/home/nayon/Desktop/accessories_shop/shop"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
